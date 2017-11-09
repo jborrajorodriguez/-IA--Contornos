a1915b119847e10d5c681a9fc6eef409815f7ba4
Aralvarellosblas <arturo.alvarellos@gmail.com>
2017-11-09 09:58:37 +0100

    Chistes implementados

--- src/ia/contornos/Mostrar.java
+++ src/ia/contornos/Mostrar.java
@@ -9,5 +9,29 @@ public class Mostrar {
     public String resposta(String nombre){
         return("Hola "+nombre+" Encantado de conocerte, mi nombre es Hope");
     }
-    
+    public void chiste(int x){
+        switch(x){
+            case 1: System.out.println("En una agencia de empleo:\n"
+                    + "- Hola, ¿tiene algún trabajo para mí?\n"
+                    + "\n"
+                    + "- Mmm, puede ser, ¿le interesa de jardinero?\n"
+                    + "\n"
+                    + "- ¿¿¿Dejar dinero??? ¡Pero si yo busco que me lo den!");
+            break;
+            case 2: System.out.println("- ¿Qué son 50 físicos y 50 químicos juntos?\n"
+                    + "- ¡100tíficos!");
+            break;
+            case 3: System.out.println("- Según su currículum, conoce 20 idiomas.\n"
+                    + "- Así es, señor. \n"
+                    + "- ¿Y cuál habla mejor? \n"
+                    + "- Hablar, hablar, ninguno. Pero sé que existen.");
+            break;
+            case 4: System.out.println("- Buenas tardes señor, soy el afinador de pianos.\n"
+                    + "- Pero yo no lo he llamado... \n"
+                    + "\n"
+                    + "- Ya, ya, me llamaron sus vecinos.");
+            break;
+            default: System.out.println("Error en la selección");
+        }
+    }
 }
