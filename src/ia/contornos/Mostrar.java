36b9ab20ed48c4d7317e990dc36af60bc48be196
Aralvarellosblas <arturo.alvarellos@gmail.com>
2017-11-09 09:18:56 +0100

    Clase terminada

--- src/ia/contornos/Mostrar.java
+++ src/ia/contornos/Mostrar.java
@@ -3,11 +3,10 @@ package ia.contornos;
  * @author Arturo
  */
 public class Mostrar {
-    private String nombre;
-
+    
     public Mostrar() {
     }
-    public String resposta(int nombre){
+    public String resposta(String nombre){
         return("Hola "+nombre+" Encantado de conocerte, mi nombre es Hope");
     }
 }
