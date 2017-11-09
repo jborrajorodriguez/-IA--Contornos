09cdf95714fcbe92f42fc7be69a70ac61d5e52b0
Aralvarellosblas <arturo.alvarellos@gmail.com>
2017-11-09 08:58:07 +0100

    Mensaje de la clase mostrar

--- src/ia/contornos/Mostrar.java
+++ src/ia/contornos/Mostrar.java
@@ -3,5 +3,11 @@ package ia.contornos;
  * @author Arturo
  */
 public class Mostrar {
+    private String nombre;
 
+    public Mostrar() {
+    }
+    public String resposta(int nombre){
+        return("Hola "+nombre+" Encantado de conocerte, mi nombre es Hope");
+    }
 }
